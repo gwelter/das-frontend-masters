ce61d6b860226d613345dfeda72fbed1850b314c
Guilherme Welter <guilherme.a.welter@gmail.com>
2025-09-03 17:59:28 -0300

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let hight = haystack.length;
 
+    do {
+        const mid = Math.floor(low + (hight - low)/2);
+        const value = haystack[mid];
+        if (value === needle) {
+            return true;
+        } else if (value > needle ) {
+            hight = mid;
+        } else {
+            low = mid+1;
+        }
+    } while (low < hight)
+    return false;
 }
